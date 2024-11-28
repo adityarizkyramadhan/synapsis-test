7b98a339e5642a83d757f755647114d49a8a7049
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2024-11-28 23:14:36 +0700

    feat: update user service methods to accept string IDs and handle conversion errors

--- user-service/internal/service/user.go
+++ user-service/internal/service/user.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"strconv"
 
 	"github.com/adityarizkyramadhan/synapsis-test/user-service/internal/model"
 	"github.com/adityarizkyramadhan/synapsis-test/user-service/internal/repository"
+	"github.com/adityarizkyramadhan/synapsis-test/user-service/utils"
 )
 
 type User struct {
@@ -15,18 +17,30 @@ func NewUser(repoUser *repository.User) *User {
 	return &User{repoUser: repoUser}
 }
 
-func (u *User) GetByID(ctx context.Context, id uint) (*model.User, error) {
-	return u.repoUser.GetByID(ctx, id)
+func (u *User) GetByID(ctx context.Context, id string) (*model.User, error) {
+	idUint, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return nil, utils.NewError(utils.ErrBadRequest, err.Error())
+	}
+	return u.repoUser.GetByID(ctx, uint(idUint))
 }
 
 func (u *User) Create(ctx context.Context, user *model.User) error {
 	return u.repoUser.Create(ctx, user)
 }
 
-func (u *User) Update(ctx context.Context, id uint, user *model.User) error {
-	return u.repoUser.Update(ctx, id, user)
+func (u *User) Update(ctx context.Context, id string, user *model.User) error {
+	idUint, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return utils.NewError(utils.ErrBadRequest, err.Error())
+	}
+	return u.repoUser.Update(ctx, uint(idUint), user)
 }
 
-func (u *User) Delete(ctx context.Context, id uint) error {
-	return u.repoUser.Delete(ctx, id)
+func (u *User) Delete(ctx context.Context, id string) error {
+	idUint, err := strconv.ParseUint(id, 10, 64)
+	if err != nil {
+		return utils.NewError(utils.ErrBadRequest, err.Error())
+	}
+	return u.repoUser.Delete(ctx, uint(idUint))
 }
