aca31b2eef7a60e82ebddf4eff59eef3d4766e82
Aditya Rizky Ramadhan <adityarizky1020@gmail.com>
2024-11-28 23:00:58 +0700

    initialize user service with Redis and PostgreSQL configurations, JWT middleware, and response utilities

---
+++ user-service/config/database/postgresql.go
@@ -0,0 +1,35 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var dbGlobal *gorm.DB
+
+func NewDB() (*gorm.DB, error) {
+	if dbGlobal != nil {
+		return dbGlobal, nil
+	}
+	var dsn string
+	var dialector gorm.Dialector
+	dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+	dialector = postgres.Open(dsn)
+	db, err := gorm.Open(dialector, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		return nil, err
+	}
+	dbGlobal = db
+	return dbGlobal, nil
+}
